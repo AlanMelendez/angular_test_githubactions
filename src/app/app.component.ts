5d146ec6353251248f118a1cecb56519da792343
Alan Cuevas Melendez <73464077+AlanMelendez@users.noreply.github.com>
2024-12-10 11:45:47 -0600

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,5 +9,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'githubactions-angular';
+  title = 'to EXAMPLE GITHUB ACTIONS';
 }
